5e27483ba95c86d0f05e02f7f5bafb96c00f6c18
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-02-11 21:50:56 +0530

    connection database and creating usermodel

---
+++ backend/src/db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+function connectDB() {
+    try {
+    mongoose.connect(process.env.MONGODB_URL)
+    console.log('Connected to MongoDB');
+    } catch (error) {
+
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+        
+    }
+}
+module.exports = connectDB;
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,34 @@
+const mongoos = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoos.Schema({
+    email:{
+        type:String,
+        required:[true,'Email is required'],
+        unique:[true,'Email must be unique'],
+        trim:true,
+        lowercase:true,
+        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,'Please fill a valid email address']
+    },
+    name:{
+        type:String,
+        required:[true,'Name is required'],
+        trim:true,
+    },
+    password:{
+        type:String,
+        required:[true,'Password is required'],
+        minlength:[6,'Password must be at least 6 characters long'],
+        select:false
+    },
+    
+},{timestamps:true})
+
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')){
+        return next();
+    }
+    const hash = await bcryot.hash(this.password,10);
+    this.password = hash;
+    next();
+})
